e92dad23b4a84a6997fcc09a4b494002bd3e08d7
stevenjokes <a867907127@gmail.com>
2020-10-09 22:41:21 +0800

    1st

--- 3.longest-substring-without-repeating-characters.py
+++ 3.longest-substring-without-repeating-characters.py
@@ -1,3 +1,17 @@
+# coding=utf-8
+'''
+version:
+Author:  StevenJokess https://github.com/StevenJokess
+Date: 2020-10-09 21:53:46
+LastEditors:  StevenJokess https://github.com/StevenJokess
+LastEditTime: 2020-10-09 22:41:11
+Description:
+TODO::
+Reference:https://leetcode.com/problems/longest-substring-without-repeating-characters/discuss/857004/O(n)-greater99.63-40ms-Readable-for-beginners
+https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/si-lu-qing-xi-yi-ci-bian-li-gao-xiao-qiu-jie-by-jo/
+https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetc-2/
+'''
+
 #
 # @lc app=leetcode id=3 lang=python3
 #
@@ -7,6 +21,21 @@
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
+                # 使用一个辅助变量来暂时存储匹配的子串
+        ans = ''
+        tep = ''
+        for i in s:
+            # 遍历，若不重复则记录该字符
+            if i not in tep:
+                tep += i
+            # 如果遇到了已经存在的字符，则找到该字符所在位置，删除该字符，并保留该位置之后的子串，并把当前字符加入到最后，完成更新
+            else:
+                tep = tep[tep.index(i)+1:]
+                tep += i
+            # 如果是当前最长的，就替换掉之前存储的最长子串
+        if len(tep) > len(ans):
+                    ans = tep
+        return len(ans)
+
 # @lc code=end
 
