9f12b2681dd6eb8c3fd60c6eb23caa5a69c4eefc
stevenjokes <a867907127@gmail.com>
2020-10-09 23:29:03 +0800

    perfect

--- 3.longest-substring-without-repeating-characters.py
+++ 3.longest-substring-without-repeating-characters.py
@@ -4,12 +4,13 @@ version:
 Author:  StevenJokess https://github.com/StevenJokess
 Date: 2020-10-09 21:53:46
 LastEditors:  StevenJokess https://github.com/StevenJokess
-LastEditTime: 2020-10-09 22:49:14
+LastEditTime: 2020-10-09 23:28:54
 Description:
 TODO::
 Reference:https://leetcode.com/problems/longest-substring-without-repeating-characters/discuss/857004/O(n)-greater99.63-40ms-Readable-for-beginners
 https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/si-lu-qing-xi-yi-ci-bian-li-gao-xiao-qiu-jie-by-jo/
 https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetc-2/
+https://www.geeksforgeeks.org/enumerate-in-python/
 '''
 
 #
@@ -21,27 +22,18 @@ https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # 哈希集合，记录每个字符是否出现过
-        occ = set()
-        n = len(s)
-        # 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-        rk, ans = -1, 0
-
-        for i in range(n):
-            # 遍历，若不重复则记录该字符
-            if i != 0:
-                # 左指针向右移动一格，移除一个字符
-                occ.remove(s[i - 1])
-            if i not in tep:
-                tep += i
-            # 如果遇到了已经存在的字符，则找到该字符所在位置，删除该字符，并保留该位置之后的子串，并把当前字符加入到最后，完成更新
+        # k右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        k, res, c_dict = -1, 0, {}
+        for i, c in enumerate(s):
+            if c in c_dict and c_dict[c] > k:  # 字符c在字典中 （即重复）且 上次出现的下标大于当前长度的起始下标
+                k = c_dict[c] #
+                c_dict[c] = i # 跳到后面
+                # print(k, c, c_dict, c_dict[c], res, i-k, "-1")
             else:
-                tep = tep[tep.index(i)+1:]
-                tep += i
-            # 如果是当前最长的，就替换掉之前存储的最长子串
-        if len(tep) > len(ans):
-                    ans = tep
-        return ans
+                c_dict[c] = i
+                res = max(res, i-k) # 如果是当前最长的，就替换掉之前的。i-k
+                # print(k, c, c_dict, c_dict[c], res, i-k, "-2")
+        return res
 
 # @lc code=end
 
