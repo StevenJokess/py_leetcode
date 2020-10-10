621ad78a23784d2025905bd8f7cc6e6597c54d4d
stevenjokes <a867907127@gmail.com>
2020-10-10 15:47:44 +0800

    Time Limit Exceeded

--- 35.search-insert-position.py
+++ 35.search-insert-position.py
@@ -4,7 +4,7 @@ version:
 Author:  StevenJokess https://github.com/StevenJokess
 Date: 2020-10-10 14:30:13
 LastEditors:  StevenJokess https://github.com/StevenJokess
-LastEditTime: 2020-10-10 15:01:28
+LastEditTime: 2020-10-10 15:46:00
 Description:
 TODO::
 Reference:index方法获取的只是第一个索引
@@ -20,29 +20,44 @@ https://blog.csdn.net/Jerry_1126/article/details/88924288
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         leng = len(nums)
-        print(leng)
         if leng == 0:
             return 0
 
-        l, r = nums[0], nums[-1]
+        l, r = 0, leng-1
 
-        middle = leng//2
-        midnum = nums[middle]
+        middle = leng//2  # 让二叉永远左边大，0--8共9个数
+
+        if leng == 1:
+            if target < nums[l]:
+                return l
+            if target == nums[l]:
+                return l
+            if target > nums[r]:
+                return r+1
 
         while l < r: # 找
-            if target == l:
-                return nums.index(l)
-            if target == r:
-                return nums.index(r)
-            if target == midnum:
+            # 边界处理
+            if target < nums[l]:
+                return l
+            if target == nums[l]:
+                return l
+            if target == nums[r]:
+                return r
+            if target > nums[r]:
+                return r+1
+            if target == nums[middle]:
                 return middle
-            if l < target < midnum:
-                middle = middle//2
-                l = nums[middle]
-            if midnum < target < r:
-                middle = (middle+leng+1)//2
-                r = nums[middle]
-        return middle
+            # ----------------------------------------------------------------
+            if nums[l] < target < nums[middle]:
+                r = middle  # 现在右边界为原来的中界
+                middle = (l+r)//2  #
+            if nums[middle] < target < nums[r]:
+                l = middle  # 现在左边界为原来的中界
+                middle = (l+r)//2
+        if target < nums[middle]:  # 小，就代替原来位置
+            return middle
+        else:
+            return middle+1  # 大，就在原来位置后面一格
 
 
 # @lc code=end
