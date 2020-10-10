38f173852e85cc92dac1b674a66a7f79ad30b41b
stevenjokes <a867907127@gmail.com>
2020-10-10 11:34:24 +0800

    final

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -4,10 +4,11 @@ version:
 Author:  StevenJokess https://github.com/StevenJokess
 Date: 2020-10-10 10:21:05
 LastEditors:  StevenJokess https://github.com/StevenJokess
-LastEditTime: 2020-10-10 11:00:58
+LastEditTime: 2020-10-10 11:34:14
 Description:
 TODO::
 Reference:D:\onedrive\leetcode\Python\1.two-sum.py
+https://leetcode.com/problems/3sum/discuss/7392/Python-easy-to-understand-solution-(O(n*n)-time).
 '''
 #
 # @lc app=leetcode id=15 lang=python3
@@ -18,15 +19,30 @@ Reference:D:\onedrive\leetcode\Python\1.two-sum.py
 # @lc code=start
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        dict = {}
+
         res = [] # List[List[int]
-        for i, num1 in enumerate(nums):
-            for j, num2 in enumerate(nums,i+1):
-                if 0 - num1 - num2 <= num1 & num1 <= num2:#保证顺序
-                    if 0  - num1 - num2 in dict:
-                        res.append([0 - num1 - num2, num1, num2])
-                    dict[num2] = j
-            dict[num1] = i
+        nums.sort()
+        leng = len(nums)
+        for i in range(leng-2):# need at least 3 numbers to continue. py2:xrange
+            if nums[i] > 0:
+                break # sum of positive will be always >0
+            if i > 0 and nums[i] == nums[i - 1]: # prevent checking duplicate again.
+                continue
+            l, r = i+1, leng-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]: #prevent checking duplicate， l < r保证了不溢出
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
         return res
 
 # @lc code=end
