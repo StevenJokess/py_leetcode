cb2c01e7a01d6862a2bdb2dfef4110583167c600
stevenjokes <a867907127@gmail.com>
2020-10-10 12:15:36 +0800

    final

--- 50.pow-x-n.py
+++ 50.pow-x-n.py
@@ -4,13 +4,14 @@ version:
 Author:  StevenJokess https://github.com/StevenJokess
 Date: 2020-10-10 11:35:17
 LastEditors:  StevenJokess https://github.com/StevenJokess
-LastEditTime: 2020-10-10 12:07:06
+LastEditTime: 2020-10-10 12:15:30
 Description:
 TODO::
 Reference:https://leetcode-cn.com/problems/powx-n/solution/kuai-su-mi-yang-ti-by-duo1j/
 https://leetcode.com/problems/powx-n/discuss/749109/Python-Recursive-Solution-Faster-than-99
 https://leetcode.com/problems/powx-n/discuss/345741/python3-Pow(xn)
 https://leetcode.com/problems/powx-n/discuss/632283/Python-Clean-solution
+实例方法不同（第一个参数为self）
 '''
 
 #
@@ -28,7 +29,7 @@ class Solution:
         else:
             return Solution.PowP(x, n)
     @staticmethod
-    def Powp(x, y):
+    def PowP(x, y):
         res = 1
         if y == 0:
             return res
