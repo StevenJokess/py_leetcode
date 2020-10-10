bfb6129644b4243355e351e6d2c177a7e6ce3f36
stevenjokes <a867907127@gmail.com>
2020-10-10 12:03:37 +0800

    2nd

---
+++ 50.pow-x-n.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+'''
+version:
+Author:  StevenJokess https://github.com/StevenJokess
+Date: 2020-10-10 11:35:17
+LastEditors:  StevenJokess https://github.com/StevenJokess
+LastEditTime: 2020-10-10 12:03:12
+Description:
+TODO::
+Reference:https://leetcode-cn.com/problems/powx-n/solution/kuai-su-mi-yang-ti-by-duo1j/
+https://leetcode.com/problems/powx-n/discuss/749109/Python-Recursive-Solution-Faster-than-99
+https://leetcode.com/problems/powx-n/discuss/345741/python3-Pow(xn)
+'''
+
+#
+# @lc app=leetcode id=50 lang=python3
+#
+# [50] Pow(x, n)
+#
+
+# @lc code=start
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+
+        if n < 0:
+            Solution.PowP(1/x, -n)
+        else:
+            Solution.PowP(x, n)
+
+        def Powp(x, y):
+            res = 1
+            if y == 0:
+                return res
+            while y > 0:
+                if y % 2 == 1 :
+                    res = res * x
+                y = y >> 1
+                x *= x
+        return res
+            # @lc code=end
+
