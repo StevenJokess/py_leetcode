9d471b1bbb09164f206b92c62d955b55a7ef337c
stevenjokes <a867907127@gmail.com>
2020-10-10 13:43:01 +0800

    Time Limit Exceeded

--- 69.sqrt-x.py
+++ 69.sqrt-x.py
@@ -4,10 +4,10 @@ version:
 Author:  StevenJokess https://github.com/StevenJokess
 Date: 2020-10-10 12:15:54
 LastEditors:  StevenJokess https://github.com/StevenJokess
-LastEditTime: 2020-10-10 13:33:45
+LastEditTime: 2020-10-10 13:42:09
 Description:
 TODO::
-Reference:
+Reference:https://leetcode.com/problems/sqrtx/discuss/871095/Python3-solution-using-binary-search
 '''
 #
 # @lc app=leetcode id=69 lang=python3
@@ -22,17 +22,19 @@ class Solution:
             return 1
 
         lower = 0
-        upper = x / 2 if x > 4 else x
-        middle = (lower + upper)/2
+        upper = x // 2 if x > 4 else x
+        middle = (lower + upper)//2
 
-        while middle*middle != x:
-            if middle * middle < x:
-                lower = (lower + middle)/2
-                middle = (lower + upper)/2
-            else:
-                upper = (upper + middle)/2
-                middle = (lower + upper)/2
-        return int(middle)
+        while True:
+            if (middle +2) * (middle +2) < x:
+                lower = (lower + middle)//2
+                middle = (lower + upper)//2
+            elif (middle -1) * (middle -1) > x:
+                upper = (upper + middle)//2
+                middle = (lower + upper)//2
+            elif (middle * middle < x) and ((middle + 1) * (middle + 1) > x):
+                break
+        return middle
 
 
 
