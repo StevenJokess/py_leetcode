c2e5897ae1d4f82cc90a02f2a15a4c9f88631d0f
stevenjokes <a867907127@gmail.com>
2020-10-10 15:01:54 +0800

    1/62 cases passed (N/A)

--- 35.search-insert-position.py
+++ 35.search-insert-position.py
@@ -4,10 +4,11 @@ version:
 Author:  StevenJokess https://github.com/StevenJokess
 Date: 2020-10-10 14:30:13
 LastEditors:  StevenJokess https://github.com/StevenJokess
-LastEditTime: 2020-10-10 14:41:52
+LastEditTime: 2020-10-10 15:01:28
 Description:
 TODO::
-Reference:
+Reference:index方法获取的只是第一个索引
+https://blog.csdn.net/Jerry_1126/article/details/88924288
 '''
 #
 # @lc app=leetcode id=35 lang=python3
@@ -18,22 +19,30 @@ Reference:
 # @lc code=start
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l, r = nums[0], nums[-1]
         leng = len(nums)
-        middle = leng//2+1
+        print(leng)
+        if leng == 0:
+            return 0
+
+        l, r = nums[0], nums[-1]
+
+        middle = leng//2
         midnum = nums[middle]
-        while l<r: # 找
+
+        while l < r: # 找
             if target == l:
-                return l
+                return nums.index(l)
             if target == r:
-                return r
+                return nums.index(r)
+            if target == midnum:
+                return middle
             if l < target < midnum:
-                middle = middle//2 +1
+                middle = middle//2
                 l = nums[middle]
             if midnum < target < r:
-                middle = (middle+leng)//2 -1
+                middle = (middle+leng+1)//2
                 r = nums[middle]
-
+        return middle
 
 
 # @lc code=end
