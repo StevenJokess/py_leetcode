c8bd3e630d85fccbfcbcdd781753b8429bdcbb08
stevenjokes <a867907127@gmail.com>
2020-10-10 13:59:41 +0800

    final

--- 69.sqrt-x.py
+++ 69.sqrt-x.py
@@ -4,7 +4,7 @@ version:
 Author:  StevenJokess https://github.com/StevenJokess
 Date: 2020-10-10 12:15:54
 LastEditors:  StevenJokess https://github.com/StevenJokess
-LastEditTime: 2020-10-10 13:44:40
+LastEditTime: 2020-10-10 13:56:33
 Description:
 TODO::
 Reference:https://leetcode.com/problems/sqrtx/discuss/871095/Python3-solution-using-binary-search
@@ -22,18 +22,17 @@ class Solution:
             return 1
 
         lower = 0
-        upper = x // 2 if x > 4 else x
+        upper = x//2 + 1 if x > 4 else x # must add 1 to make upper big enough
         middle = (lower + upper)//2
 
         while True:
             if (middle * middle <= x) and ((middle + 1) * (middle + 1) > x) :
                 break
-            elif (middle -1) * (middle -1) > x:
-                upper = (upper + middle)//2
-                middle = (lower + upper)//2
-            elif (middle +2) * (middle +2) < x:
-                lower = (lower + middle)//2
-                middle = (lower + upper)//2
+            elif middle * middle > x:
+                upper = middle
+            elif (middle+1) * (middle+1) <= x:
+                lower = middle
+            middle = (lower + upper)//2
         return middle
 
 
