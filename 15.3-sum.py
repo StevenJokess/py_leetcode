415129f64ca2753bfc096bd986bfc29ae8227cf9
stevenjokes <a867907127@gmail.com>
2020-10-10 10:29:23 +0800

    first test

---
+++ 15.3-sum.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+'''
+version:
+Author:  StevenJokess https://github.com/StevenJokess
+Date: 2020-10-10 10:21:05
+LastEditors:  StevenJokess https://github.com/StevenJokess
+LastEditTime: 2020-10-10 10:28:40
+Description:
+TODO::
+Reference:D:\onedrive\leetcode\Python\1.two-sum.py
+'''
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        dict = {}
+        for i, num1 in enumerate(nums):
+            for j, num2 in enumerate(nums):
+                if 0  - num1 - num2 in dict:
+                    return [dict[0 - num1 - num2], i, j]
+                dict[num2] = j
+            dict[num1] = i
+        return []
+
+# @lc code=end
+
